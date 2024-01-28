7f67b0192e5dd943a2c22cf7cc7799a55827ba23
csslick <digitone@naver.com>
2024-01-29 00:36:47 +0900

    vue3-comp-api

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { ref, onMounted } from 'vue';
+
+  let count = ref(0); // 상태변수 초기값(state)
+  let title = "Hello Vue";
+
+  onMounted(() => {
+    console.log('mounted');
+  })
+
+  const increaseCount = () => {
+    count.value++;
+  }
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="increaseCount">count++</button>
+    <p>{{ count }}</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
