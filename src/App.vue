bc36df3263c1e9940ff37b6376208ad1bb3dcc6f
zzimni <hjm3327@naver.com>
2024-06-10 23:42:38 +0900

    2-24 강의노트 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
+// option api -> composition api
+// - setup 기능으로 스크립트 작성 규칙을 단순화
+// - 옵션을 따로 구분할 필요없이 한 곳에서 작성
+// - ref()로 상태변수 관리
+// - defineProps로 prop 관리
 
-  let count = ref(0); // 상태변수 초기값(state)
-  let title = "Hello Vue";
+import { ref, onMounted } from "vue";
 
-  onMounted(() => {
-    console.log('mounted');
-  })
+let count = ref(0); // 상태변수 초기값(state)
+let title = "Hello Vue"; // 일반 변수
 
-  const increaseCount = () => {
-    count.value++;
-  }
+onMounted(() => {
+  // composition api에서는 라이프사이클 훅을 사용하려면 모듈을 import부터 해줘야함
+  console.log("mounted");
+});
+
+const increaseCount = () => {
+  count.value++; // count = ref(0); 으로 관리되고있으므로 실제 값은 ref함수 안에 들어있기 때문에 count 숫자를 변경할 때는 그냥 count가 아닌 count.value로 접근해야함 (함수에서 value라는 값을 가져와야한다)
+};
 </script>
 
 <template>
@@ -21,5 +28,4 @@
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
